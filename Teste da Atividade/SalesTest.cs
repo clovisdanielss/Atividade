369e810363bcc31e2eaf7ef2c1dae755bf78577a
Clóvis <clovisdanielss@gmail.com>
2022-10-07 15:15:29 -0300

    refatorando, retirando algumas más praticas detectadas na entrevista

--- Teste da Atividade/SalesTest.cs
+++ Teste da Atividade/SalesTest.cs
@@ -1,7 +1,7 @@
 
-using Atividade.Exceptions;
 using Atividade.Models;
 using Atividade.Repository;
+using Atividade.Services;
 
 namespace Teste_da_Atividade
 {
@@ -60,14 +60,8 @@ namespace Teste_da_Atividade
                     },
                     SalesDate = DateTime.Now
                 };
-                try
-                {
-                    sale = sales.Create(sale);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new SalesmanMustNotBeNullException().Message);
-                }
+                sale = sales.Create(sale);
+                Assert.IsNull(sale);
             }
         }
         [TestMethod]
@@ -90,14 +84,8 @@ namespace Teste_da_Atividade
                     },
                     SalesDate = DateTime.Now
                 };
-                try
-                {
-                    sale = sales.Create(sale);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new AtLeastOneItemRequiredException().Message);
-                }
+                sale = sales.Create(sale);
+                Assert.IsNull(sale);
             }
         }
 
@@ -118,14 +106,8 @@ namespace Teste_da_Atividade
         {
             using (SalesRepository sales = new SalesRepository())
             {
-                try
-                {
-                    var saleFound = sales.GetById("Qualquer Coisa");
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ItemNotFoundException().Message);
-                }
+                var saleFound = sales.GetById("Qualquer Coisa");
+                Assert.IsNull(saleFound);
             }
         }
         [TestMethod]
@@ -133,111 +115,53 @@ namespace Teste_da_Atividade
         {
             var sale = CreateSale();
             Assert.IsTrue(sale.Status == SaleStatus.WAITING_PAYMENT);
-            using (SalesRepository sales = new SalesRepository())
-            {
-                sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
-                Assert.IsTrue(sale.Status == SaleStatus.PAYMENT_APPROVED);
-                sales.ChangeStatus(sale, SaleStatus.SENT);
-                Assert.IsTrue(sale.Status == SaleStatus.SENT);
-                sales.ChangeStatus(sale, SaleStatus.DELIVERED);
-                Assert.IsTrue(sale.Status == SaleStatus.DELIVERED);
+            var sales = new SalesService();
+            sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
+            Assert.IsTrue(sale.Status == SaleStatus.PAYMENT_APPROVED);
+            sales.ChangeStatus(sale, SaleStatus.SENT);
+            Assert.IsTrue(sale.Status == SaleStatus.SENT);
+            sales.ChangeStatus(sale, SaleStatus.DELIVERED);
+            Assert.IsTrue(sale.Status == SaleStatus.DELIVERED);
 
-                sale = CreateSale();
-                sales.ChangeStatus(sale, SaleStatus.CANCELLED);
-                Assert.IsTrue(sale.Status == SaleStatus.CANCELLED);
+            sale = CreateSale();
+            sales.ChangeStatus(sale, SaleStatus.CANCELLED);
+            Assert.IsTrue(sale.Status == SaleStatus.CANCELLED);
 
-                sale = CreateSale();
-                sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
-                Assert.IsTrue(sale.Status == SaleStatus.PAYMENT_APPROVED);
-                sales.ChangeStatus(sale, SaleStatus.CANCELLED);
-                Assert.IsTrue(sale.Status == SaleStatus.CANCELLED);
-            }
+            sale = CreateSale();
+            sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
+            Assert.IsTrue(sale.Status == SaleStatus.PAYMENT_APPROVED);
+            sales.ChangeStatus(sale, SaleStatus.CANCELLED);
+            Assert.IsTrue(sale.Status == SaleStatus.CANCELLED);
         }
+
         [TestMethod]
         public void ChangeStatus_FailureOnly()
         {
             var sale = CreateSale();
             Assert.IsTrue(sale.Status == SaleStatus.WAITING_PAYMENT);
-            using (SalesRepository sales = new SalesRepository())
+            var sales = new SalesService();
+            //WAITING_PAYMENT
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.SENT));
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.DELIVERED));
+
+            //PAYMENT_APPROVED
+            sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.WAITING_PAYMENT));
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.DELIVERED));
+            sales.ChangeStatus(sale, SaleStatus.SENT);
+
+            //SENT
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.WAITING_PAYMENT));
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.CANCELLED));
+            Assert.IsFalse(sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED));
+
+            //DELIVERED
+            sales.ChangeStatus(sale, SaleStatus.DELIVERED);
+            for (int i = 0; i < 5; i++)
             {
-                //WAITING_PAYMENT
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.SENT);
-                }
-                catch(Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.SENT).Message);
-                }
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.DELIVERED);
-                }
-                catch(Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.DELIVERED).Message);
-                }
-
-                //PAYMENT_APPROVED
-                sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.WAITING_PAYMENT);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.WAITING_PAYMENT).Message);
-                }
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.DELIVERED);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.DELIVERED).Message);
-                }
-
-                //SENT
-                sales.ChangeStatus(sale, SaleStatus.SENT);
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.WAITING_PAYMENT);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.WAITING_PAYMENT).Message);
-                }
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.CANCELLED);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.CANCELLED).Message);
-                }
-                try
-                {
-                    sales.ChangeStatus(sale, SaleStatus.PAYMENT_APPROVED);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, SaleStatus.PAYMENT_APPROVED).Message);
-                }
-
-                //DELIVERED
-                sales.ChangeStatus(sale, SaleStatus.DELIVERED);
-                for(int i=0; i<5; i++)
-                {
-                    try
-                    {
-                        sales.ChangeStatus(sale, (SaleStatus)i);
-                    }
-                    catch (Exception ex)
-                    {
-                        Assert.IsTrue(ex.Message == new ImpossibleToChangeStatusException(sale.Status, (SaleStatus)i).Message);
-                    }
-                }
+                Assert.IsFalse(sales.ChangeStatus(sale, (SaleStatus)i));
             }
+
         }
 
         [TestMethod]
@@ -248,7 +172,7 @@ namespace Teste_da_Atividade
                 var sls = sales.GetAll();
                 while (sls.Count > 0)
                     sales.DeleteById(sls[0].Id);
-                for (int i =0; i< 10; i++)
+                for (int i = 0; i < 10; i++)
                 {
                     sls = sales.GetAll();
                     Assert.IsNotNull(sls);
